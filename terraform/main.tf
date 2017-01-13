6e61f8186b989b1529d4dd59eac902b844d52960
Jesse R. Adams <jesse.adams@nike.com>
2017-01-13 10:43:13 -0800

    Finishing touches

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,11 +25,12 @@ resource "aws_elb" "web-elb" {
 
 resource "aws_autoscaling_group" "web-asg" {
   availability_zones   = ["${split(",", var.availability_zones)}"]
-  name                 = "simple-web-asg"
+  name                 = "simple-web-asg-${aws_launch_configuration.web-lc.name}"
   max_size             = "${var.asg_max}"
   min_size             = "${var.asg_min}"
   desired_capacity     = "${var.asg_desired}"
   force_delete         = true
+  wait_for_elb_capacity = "${var.asg_desired}"
   launch_configuration = "${aws_launch_configuration.web-lc.name}"
   load_balancers       = ["${aws_elb.web-elb.name}"]
   tag {
@@ -37,14 +38,21 @@ resource "aws_autoscaling_group" "web-asg" {
     value               = "web-asg"
     propagate_at_launch = "true"
   }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_launch_configuration" "web-lc" {
-  name          = "simple-web-lc"
+  name_prefix          = "simple-web-lc-"
   image_id      = "${var.ami}"
   instance_type = "${var.instance_type}"
   security_groups = ["${aws_security_group.simple.id}"]
   key_name        = "${var.key_name}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_security_group" "simple" {
